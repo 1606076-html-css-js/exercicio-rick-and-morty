5761c25d6ad3edee256e3f2dfec47261868f989f
SabinoWS <sabino.th@hotmail.com>
2023-03-07 21:19:44 -0300

    Adiciona mais dicas

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+executar()
+
 // 0 - Adicionar evento em botão para executar contexto da função
 async function executar(){
-    // 1 - Pegar id do input
-    // 2 - Buscar personagem da api
+    // 1 - Pegar o valor digitado input (id do personagem)
     const id = 1 // ! Substituir pelo valor do INPUT !
+    // 2 - Buscar personagem da API (já está pronto)
     const personagem = await buscarPersonagemDaAPI(id)
-    console.log(personagem)
 
     /* MONTAR ABAIXO HTML COM O VALOR RETORNADO DA API */
     // 3 - Pegar elemento de resultado do HTML
     // 4 - Atribuir valores do personagem na tela
+    // 4.1 - Mudar o atributo src da tag img
+    // 4.2 - Mudar o atributo innerText da tag que guarda o nome do personagem
 
     /* Dica:
         Dar console.log na variável 'personagem'
